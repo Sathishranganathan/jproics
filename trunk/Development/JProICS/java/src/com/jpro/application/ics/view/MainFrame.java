ebec9152f88121145a99895d558707a73f0301d2
dilan.hansike <dilan.hansike@07d8fd48-6ff6-7e51-b3e8-f7a813daa9b3>
2010-10-12 10:46:52 +0000

    Java Project Added

--- trunk/Development/JProICS/java/src/com/jpro/application/ics/view/MainFrame.java
+++ trunk/Development/JProICS/java/src/com/jpro/application/ics/view/MainFrame.java
@@ -1,12 +1,39 @@
 package com.jpro.application.ics.view;
 
+import java.awt.BorderLayout;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JRootPane;
+import javax.swing.JToolBar;
+
 public class MainFrame {
 
-	/**
-	 * @param args
-	 */
+	JFrame frame;
+
+	public MainFrame() {
+		frame = new JFrame("JPro");
+		JToolBar toolbar = new JToolBar("Toolbar", JToolBar.VERTICAL);
+		JButton cutbutton = new JButton("CUT");
+		toolbar.add(cutbutton);
+		JButton copybutton = new JButton("COPY");
+		toolbar.add(copybutton);
+		JButton pastebutton = new JButton("PASTE");
+		toolbar.add(pastebutton);
+		
+		frame.getContentPane().add(toolbar, BorderLayout.WEST);
+		frame.setUndecorated(true);
+		frame.getRootPane().setWindowDecorationStyle(JRootPane.PLAIN_DIALOG);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000, 800);
+		frame.setVisible(true);
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		new MainFrame();
 
 	}
 
