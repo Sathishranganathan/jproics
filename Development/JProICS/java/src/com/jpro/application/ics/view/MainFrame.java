d001527b5d37ea719a5295d60baf9cf0092ac6d9
dilan.hansike <dilan.hansike@07d8fd48-6ff6-7e51-b3e8-f7a813daa9b3>
2010-10-12 09:52:39 +0000

    Java Project Added

---
+++ Development/JProICS/java/src/com/jpro/application/ics/view/MainFrame.java
@@ -0,0 +1,13 @@
+package com.jpro.application.ics.view;
+
+public class MainFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
