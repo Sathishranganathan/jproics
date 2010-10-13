682822be2e60a4d3f0efac1bf69ca5c5b038976b
dilan.hansike <dilan.hansike@07d8fd48-6ff6-7e51-b3e8-f7a813daa9b3>
2010-10-13 12:44:55 +0000

    Added JMenu

--- Development/JProICS/java/src/com/jpro/application/ics/view/MainFrame.java
+++ Development/JProICS/java/src/com/jpro/application/ics/view/MainFrame.java
@@ -2,27 +2,49 @@ package com.jpro.application.ics.view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Event;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JRootPane;
 import javax.swing.JToolBar;
+import javax.swing.KeyStroke;
 
 public class MainFrame {
 
-	JFrame frame;
+	private JFrame frame;
+	private JMenuBar bar;
+	private JMenu mnuFile;
+
+	private JMenuItem newMenuItem1;
 
 	public MainFrame() {
 
 		frame = new JFrame("JPro");
+
+		// Creating the MenuBar For Displaying All the Menus of Program.
+
+		bar = new JMenuBar(); // Creating the MenuBar Object.
+		frame.setJMenuBar(bar); // Setting Main Window MenuBar.
+
+		mnuFile = new JMenu("File");
+		mnuFile.setMnemonic((int) 'F');
+		createFileMenu(mnuFile);
+
+		bar.add(mnuFile);
+
 		JToolBar toolbar = new JToolBar("Toolbar", JToolBar.VERTICAL);
-		JButton cutbutton = new JButton("CUT");
-		toolbar.add(cutbutton);
-		JButton copybutton = new JButton("COPY");
-		toolbar.add(copybutton);
-		JButton pastebutton = new JButton("PASTE");
-		toolbar.add(pastebutton);
+		JButton button1 = new JButton("Test 1");
+		toolbar.add(button1);
+		JButton button2 = new JButton("Test 2");
+		toolbar.add(button2);
+		JButton button3 = new JButton("Test 3");
+		toolbar.add(button3);
 
 		frame.getContentPane().add(toolbar, BorderLayout.WEST);
 		frame.setUndecorated(true);
@@ -42,6 +64,15 @@ public class MainFrame {
 
 		// Move the window
 		frame.setLocation(x, y);
+
+	}
+
+	private void createFileMenu(JMenu mnuFile2) {
+
+		newMenuItem1 = new JMenuItem("New");
+		newMenuItem1.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N,
+				Event.CTRL_MASK));
+		mnuFile2.add(newMenuItem1);
 
 	}
 
