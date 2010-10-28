e4fbd343d58d7d882b655be28c6950a6467dbe76
dilan.hansike <dilan.hansike@07d8fd48-6ff6-7e51-b3e8-f7a813daa9b3>
2010-10-28 12:56:58 +0000

    Tested some Swing and UI

--- trunk/Development/JProICS/java/src/com/jpro/application/ics/view/SummaryPanel.java
+++ trunk/Development/JProICS/java/src/com/jpro/application/ics/view/SummaryPanel.java
@@ -1,8 +1,13 @@
 package com.jpro.application.ics.view;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class SummaryPanel extends JPanel{
+	
+	private JLabel test1label;
+
 	
 	public SummaryPanel() {
 		// TODO Auto-generated constructor stub
@@ -11,6 +16,10 @@ public class SummaryPanel extends JPanel{
 	
 	private void initComponents() {
 		
+		test1label = new JLabel("testa");
+		add(test1label);
+		
+		setSize(400, 600);
 		
 	}
 
